33148c781cb22a960fba2385952e9d3a23f2ecdd
pedrohcfernandes1-cmd <pedro.hcfernandes1@gmail.com>
2026-02-11 18:08:49 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-asdas
+// Váriaveis
+let vitorias = 10
+let derrotas = 5
+let rank = ""
+
+let saldo = vitorias - derrotas
+
+// Estrutura de decisão
+if (saldo <= 10) {
+    rank = "Ferro"
+}
+else if (saldo >= 11 && saldo <= 20) {
+    rank = "Bronze"
+}
+else if (saldo >= 21 && saldo <= 50) {
+    rank = "Prata"
+}
+else if (saldo >= 51 && saldo <= 80) {
+    rank = "Ouro"
+}
+else if (saldo >= 81 && saldo <= 90) {
+    rank = "Diamante"
+}
+else if (saldo >= 91 && saldo <= 100) {
+    rank = "Lendário"
+}
+else if (saldo >= 101) {
+    rank = "Imortal"
+}
+
+// Saída
+console.log("O Herói tem de saldo de vitorias: " + saldo + ". E ele está no nível de " + rank)
